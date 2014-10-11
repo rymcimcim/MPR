67bf806f53ac60886d284ffbc55ba5cc3d4d2283
s11869 <s11869@lab4l-16.pjwstk.edu.pl>
2014-10-11 17:39:05 +0200

    zmiana4

--- src/domain/Role.java
+++ src/domain/Role.java
@@ -6,4 +6,16 @@ public class Role {
 	
 	private String name;
 	private List<Privilage> privilage;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Privilage> getPrivilage() {
+		return privilage;
+	}
+	public void setPrivilage(List<Privilage> privilage) {
+		this.privilage = privilage;
+	}
 }
