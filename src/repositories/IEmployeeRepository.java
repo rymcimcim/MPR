e6b421c5180d50308fb4d187a7a2bfd2146f20fb
s11869 <s11869@lab4l-20.pjwstk.edu.pl>
2014-10-25 17:08:39 +0200

    a

--- src/repositories/IEmployeeRepository.java
+++ src/repositories/IEmployeeRepository.java
@@ -9,7 +9,7 @@ public interface IEmployeeRepository extends IRepository<Employee>{
 
 	/** 
 	 * 
-	 * Metoda wyci¹ga pracowników po roli
+	 * Metoda wyciÄ…ga pracownikÃ³w po roli
 	 * */
 	public List<Employee> withRole(Role role);
 	public List<Employee> withRole(String roleName);
